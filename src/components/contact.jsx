200bd323e2fecef1700a7403e793af5ee64b4cb9
arghyasahoo <arghyasahoo001@gmail.com>
2021-11-17 11:28:55 +0530

    Updated contact button and navbar link

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -4,7 +4,7 @@ class Contact extends Component {
     render() {
         return (
             <a class="mailto" href="mailto:ctf.bcdehl@gmail.com">
-                <div class="container-send_email">
+                <div id="contact" class="container-send_email">
                     <button class="send_email">
                         Get in Touch
                     </button>
